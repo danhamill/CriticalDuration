03874bb853152da1098ce7a2a343337369b5ab98
danhamill <hamill.daniel@gmail.com>
2026-04-30 16:23:38 -0700

    Migrate Package to UV

--- docs/conf.py
+++ docs/conf.py
@@ -4,15 +4,15 @@ sys.path.insert(0, os.path.abspath('../'))
 
 # Project information
 project = 'Critical Duration Analysis'
-copyright = '2025, Your Name'
-author = 'Your Name'
+copyright = '2025, MIT'
+author = 'Daniel Hamill'
 
 # General configuration
 extensions = [
-    'sphinx.ext.autodoc',
     'sphinx.ext.napoleon',
     'sphinx.ext.viewcode',
     'sphinx.ext.mathjax',
+    'autoapi.extension'
 ]
 
 templates_path = ['_templates']
@@ -21,3 +21,4 @@ exclude_patterns = []
 # HTML output
 html_theme = 'sphinx_rtd_theme'
 html_static_path = ['_static']
+autoapi_dirs = ['../critical_duration']
